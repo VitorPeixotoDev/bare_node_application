5b72ee3d5d9d9a858cf10b539568b8a6ea126c82
VitorPeixoto <vitorpeixotoc137@gmail.com>
2023-08-04 20:38:57 -0300

    create an account and create a file with customers ok!

--- index.js
+++ index.js
@@ -22,9 +22,56 @@ const operation = () => {
     }])
     .then(answer => {
         const action = answer['action']
-        console.log(action)
+
+        switch(action){
+            case 'Criar Conta':
+                createAnAccount()
+                break
+            default:
+                console.log('Por favor reinicie a operação.')
+        }
+    })
+    .catch(err => console.log(err))
+}
+operation()
+
+// create an account
+const createAnAccount = () => {
+    console.log(chalk.bgGreen.black('Obrigado por escolher o Account!'))
+    console.log(chalk.green('Por favor defina as opções da sua conta:'))
+
+    buildAccount()
+}
+
+const buildAccount = () => {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Digite um nome de acesso para sua conta:'
+    }])
+    .then(answer => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Essa conta já existe. Escolha outro nome.'))
+            buildAccount()
+            return
+        }
+        
+        fs.writeFileSync(
+            `accounts/${accountName}.json`, 
+            '{"balance": 0}',
+            err => console.log(err)
+        )
+
+        console.log(chalk.green(`Parabéns ${accountName}, sua conta foi criada com sucesso!`))
+        operation()
     })
     .catch(err => console.log(err))
 }
 
-operation()
